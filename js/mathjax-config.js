0e731c047b84d72ecfa34e59570858e2b6188cc1
Garrett Goon <garrett361@gmail.com>
2020-02-25 16:29:55 -0500

    first commit

---
+++ js/mathjax-config.js
@@ -0,0 +1,15 @@
+window.MathJax = {
+    tex: {
+      inlineMath: [['$', '$'], ['\\(', '\\)']],
+      macros: {
+          rd: "{\\rm d}",
+          bfk: "\\mathbf{k}",
+          bfx: "\\mathbf{x}",
+          bfp: "\\mathbf{p}",
+          bfy: "\\mathbf{y}",
+      },
+    },
+    svg: {
+      fontCache: 'global'
+    }
+  };
