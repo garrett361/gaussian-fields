4047cd8ede07370eb0fa467e57e13ee4c5d1d1e6
Garrett <garrett361@gmail.com>
2021-07-26 14:26:36 -0400

    generated html from distill ejs

---
+++ ejs_to_html.js
@@ -0,0 +1,17 @@
+var fs = require('fs'),
+    ejs = require("ejs");
+
+function ejs2html(path, information) {
+    fs.readFile(path+"/src/index.ejs", 'utf8', function (err, data) {
+        if (err) { console.log(err); return false; }
+        var ejs_string = data,
+            template = ejs.compile(ejs_string),
+            html = template(information);
+        fs.writeFile(path+"/index.html", html, function(err) {
+            if(err) { console.log(err); return false }
+            return true;
+        });  
+    });
+}
+
+ejs2html(__dirname)
